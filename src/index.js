0d403ff0ff1ab0b3e3f63968ac89c3076902d414
dalquijayg <dalquijayg@miumg.edu.gt>
2024-09-07 23:25:33 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron');
+
+if (process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {});
+}
+
+const path = require('path');
+const {autoUpdater, AppUpdater} = require('electron-updater')
+const log = require('electron-log')
+log.transports.file.resolvePathFn =()=> path.join('C:/Users/Dennis/Desktop/Vacas RRHH', 'logs/main.log');
+log.info('Hola, log');
+log.warn('prueba');
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        icon: path.join(__dirname, 'Logo-Recursos.ico'),
+        autoHideMenuBar: true
+    });
+    autoUpdater.checkForUpdatesAndNotify();
+    mainWindow.maximize(); 
+    mainWindow.loadURL(`file://${__dirname}/Vistas/Autenticacion.html`);
+});
+
+autoUpdater.on("update-available",()=>{
+    log.info("update-available")
+})
+
+autoUpdater.on("checking-for-update",()=>{
+    log.info("checking-for-update")
+})
+autoUpdater.on("download-progress",()=>{
+    log.info("download-progress")
+})
+autoUpdater.on("update-dowloaded",()=>{
+    log.info("update-dowloaded")
+})
